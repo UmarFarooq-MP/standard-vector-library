eed34574d62c68646b73b800e5c780ee98421faf
UmarFarooq-MP <umarfarooq.waheed@gmail.com>
2020-07-01 16:53:34 +0500

    more-functions added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "vector.h"
-int main (){
-    svl::vector<int> test(5);
 
+int main (){
+
+    svl::vector<int> test(5);
     std::cout << "-------------------------------------\n";
     std::cout << "size = " <<test.size() << std::endl;
     std::cout << "capacity = "<< test.capacity() << std::endl;
     test.assign(5,10);
+    test.push_back(12);
     test.display();
-    std::cout << "-------------------------------------\n";
-    std::cout <<"size = "<< test.size() << std::endl;
-    std::cout <<"capacity = "<< test.capacity() << std::endl;
-    test.assign(5,12);
+    std::cout << "size = " <<test.size() << std::endl;
+    std::cout << "capacity = "<< test.capacity() << std::endl;
+    std::cout << "at : " << test[5] <<"\n";
+    std::cout <<"********************************************\n";
+    test[5]= 111;
     test.display();
+
 }
--- vector.h
+++ vector.h
@@ -19,8 +19,7 @@ namespace svl {
         size_u m_size;
         size_u m_capacity;
         T *m_data;
-        T *m_rear;
-
+        T *m_rear; //end
 
         void reAllocate() {
             m_capacity = m_size * 2;
@@ -31,67 +30,79 @@ namespace svl {
             std::copy(m_data, m_data + m_size, temp);
             delete[] m_data;
             m_data = temp;
-            m_rear = m_data + (m_size - 1);
+            m_rear = m_data + (m_capacity - 1);
         }
 
     public:
-
         explicit vector(size_u capacity = 0) : m_capacity(capacity) {
             m_size = 0;
             m_data = new T[capacity];
             m_rear = m_data;
         }
-
         ~vector() {
             delete[] m_data;
         }
-
-        void assign(const size_u count,const T value){
-            while((m_size + count) > m_capacity){
+        void assign(const size_u count, const T value) {
+            while ((m_size + count) > m_capacity) {
                 reAllocate();
             }
-            for (size_u i{0}; i < count ; ++i,++m_size){
+            for (size_u i{0}; i < count; ++i, ++m_size) {
                 m_data[m_size] = value;
-                m_rear = m_data+m_size;
+                m_rear = m_data + m_size;
             }
         }
-
+        T *at(const T index) {
+            if (index > 0 && index < m_size - 1)
+                return m_data + index;
+            throw std::out_of_range("index out of rang");
+        }
+        T &operator[](int index){
+            if (index <0 || index >= m_size){
+                throw std::out_of_range("out of index");
+            }
+            return m_data[index];
+        }
+        T back() const{
+            return m_data[m_size-1];
+        }
         size_u capacity() const {
             return m_capacity;
         }
-
-        size_u size() const{
+        size_u size() const {
             return m_size;
         }
-
         void push_back(const T value) {
+            if (m_size == m_capacity){
+                reAllocate();
+            }
+            m_data[m_size] = value;
+            ++m_size;
         }
-
-        void display(){
-            for (int i = 0; i < m_size ; ++i){
+        void display() {
+            for (int i = 0; i < m_size; ++i) {
                 std::cout << m_data[i] << std::endl;
             }
+        }
+
+        void clear(){
+            delete [] m_data;
+            m_size = 0;
+            m_capacity = 0;
+            m_rear = nullptr;
         }
 
         T *begin() const {
             return m_data;
         }
-
         T *end() const {
             return m_rear;
         }
-
         T *rbegin() const {
             return m_data;
         }
-
         T *rend() const {
             return m_rear;
         }
-
-
     };
 }
-
-
 #endif //VECTOR_VECTOR_H
